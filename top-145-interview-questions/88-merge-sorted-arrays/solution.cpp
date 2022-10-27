ab7d4d6b491918be08fa67cc0ef161e59f1aff39
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2022-10-27 13:24:06 +0530

    solved: 0088-merge-sorted-array completed.

---
+++ top-145-interview-questions/88-merge-sorted-arrays/solution.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+class Timer {
+  high_resolution_clock::time_point start;
+  high_resolution_clock::duration duration;
+
+ public:
+  high_resolution_clock::time_point startClock() {
+    this->start = high_resolution_clock::now();
+    return this->start;
+  }
+  high_resolution_clock::rep stopClock() {
+    high_resolution_clock::time_point stop = high_resolution_clock::now();
+    this->duration = duration_cast<microseconds>(stop - this->start);
+    return this->duration.count();
+  }
+};
+class Output {
+ public:
+  template <typename T>
+  static void vector(vector<T> a) {
+    for (auto it = a.begin(); it != a.end(); it++) {
+      if (it == a.end() - 1) {
+        cout << *it;
+      } else {
+        cout << *it << "\t";
+      }
+    }
+  }
+};
+
+class Solution {
+ public:
+  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i1 = m - 1, i2 = n - 1;
+    for (int i = n + m - 1; i >= 0; i--) {
+      if ((i2 < 0) || (i1 >= 0 && nums1[i1] > nums2[i2])) {
+        nums1[i] = nums1[i1];
+        i1--;
+      } else {
+        nums1[i] = nums2[i2];
+        i2--;
+      }
+    }
+  }
+};
+
+int test(vector<int>& x, int m, vector<int>& y, int n) {
+  Solution s = Solution();
+  Timer t = Timer();
+  int duration;
+
+  cout << "\nX (m = " << m << ") = \t";
+  Output::vector(x);
+  cout << endl;
+  cout << "Y (n = " << n << ") = \t";
+  Output::vector(y);
+  cout << endl;
+
+  t.startClock();
+  s.merge(x, m, y, n);
+  duration = t.stopClock();
+
+  cout << "\tMerged Array, X = \t";
+  Output::vector(x);
+  cout << "\t\t\t\tTime Taken: " << duration << endl;
+
+  return 0;
+}
+int main() {
+  vector<int> nums1, nums2;
+  int m, n;
+
+  m = 3;
+  nums1 = {1, 2, 3, 0, 0, 0};
+  n = 3;
+  nums2 = {2, 5, 6};
+  test(nums1, m, nums2, n);
+
+  m = 1;
+  nums1 = {1};
+  n = 0;
+  nums2 = {};
+  test(nums1, m, nums2, n);
+
+  m = 0;
+  nums1 = {0};
+  n = 1;
+  nums2 = {1};
+  test(nums1, m, nums2, n);
+  return 0;
+}
