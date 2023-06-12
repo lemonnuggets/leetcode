8b1f476146e2e5704d33317b62d98e48cc69983d
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2023-06-12 13:32:22 +0530

    refactor: measureMethodPerformance calls measureFunctionPerformance instead of reimplementing call mechanism

--- modules/perf.h
+++ modules/perf.h
@@ -3,30 +3,15 @@
 template <typename Func, typename Obj, typename... Args>
 auto measureMethodPerformance(int iterations, Func&& func, Obj&& obj,
                               Args&&... args) {
-  Timer timer;
-  double totalTime = 0.0;
-  using ReturnType =
-      decltype((std::forward<Obj>(obj).*func)(std::forward<Args>(args)...));
+  // Wrap the method call in a lambda function
+  auto methodCall = [&](auto&&... capturedArgs) {
+    return (std::forward<Obj>(obj).*std::forward<Func>(func))(
+        std::forward<decltype(capturedArgs)>(capturedArgs)...);
+  };
 
-  ReturnType result;
-
-  // Warm-up phase
-  for (int i = 0; i < iterations; i++) {
-    timer.startClock();
-    result = (std::forward<Obj>(obj).*func)(std::forward<Args>(args)...);
-    timer.stopClock();
-  }
-
-  // Actual measurement phase
-  for (int i = 0; i < iterations; i++) {
-    timer.startClock();
-    result = (std::forward<Obj>(obj).*func)(std::forward<Args>(args)...);
-    double elapsed = static_cast<double>(timer.stopClock());
-    totalTime += elapsed;
-  }
-
-  double averageTime = totalTime / iterations;
-  return std::make_pair(averageTime, result);
+  // Call measureFunctionPerformance with the lambda function
+  return measureFunctionPerformance(iterations, methodCall,
+                                    std::forward<Args>(args)...);
 }
 
 template <typename Func, typename... Args>
