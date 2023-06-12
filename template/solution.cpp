b20ab51774eefc453285c4a9e2259908bb3001b6
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2023-06-12 11:22:16 +0530

    feat: added template

---
+++ template/solution.cpp
@@ -0,0 +1,35 @@
+#include "../modules/index.h"
+#define ITERATIONS 50
+class Solution {
+ public:
+  void func(void) {}
+};
+class Optimal {
+ public:
+  void func(void) {}
+};
+int test(string argument) {
+  Solution s = Solution();
+  Optimal o = Optimal();
+  Timer t = Timer();
+
+  cout << "\nArgument (size = " << argument.size() << ") = \t";
+  cout << argument << endl;
+
+  auto result =
+      measureMethodPerformance(ITERATIONS, &Solution::func, s, argument);
+  cout << "\tFunc (mine), result = \t";
+  cout << result.second << endl;
+  cout << "\t\t\t\tTime Taken: " << result.first << endl;
+
+  result = measureMethodPerformance(ITERATIONS, &Optimal::func, o, argument);
+  cout << "\tFunc (optimal), result = \t";
+  cout << result.second << endl;
+  cout << "\t\t\t\tTime Taken: " << result.first << endl;
+  return 0;
+}
+int main() {
+  string x = "Hello World!";
+  test(x);
+  return 0;
+}
