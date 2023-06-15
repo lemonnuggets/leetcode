0e4a789e9e5a51ada3c27821a44296280bb7ceec
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2023-06-15 13:04:49 +0530

    fix: updated INVALID_VALUE for 1161

--- modules/constants.h
+++ modules/constants.h
@@ -1,4 +1,7 @@
 // For Question 94
 // can use -10000 to represent null since it will be
 // an illegal value (node.val has to be b/w -100 and 100)
-#define INVALID_VALUE -10000
+// For Question 94
+// can use -100001 to represent null since it will be
+// an illegal value (node.val has to be b/w -10^5 and 10^5)
+#define INVALID_VALUE -100001
