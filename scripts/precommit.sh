834c1f81ca8908edcc10e5d248a760b3e677dcce
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2023-07-18 06:18:39 +0530

    fix: add generated grid after creating

--- scripts/precommit.sh
+++ scripts/precommit.sh
@@ -3,5 +3,6 @@
 source ./venv/bin/activate
 
 ls .. -l | grep -P '\d{4}-' --only-matching | sed 's/-//g' | ./generate_grid.py -n 3000 -o ../assets/grid.png -r 2
+git add ../assets/grid.png
 
 echo "> Grid generated."
