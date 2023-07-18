f9c54dacc0c79f8ebc5ce176d631c0b46b0106cb
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2023-07-18 06:26:49 +0530

    feat: better output on precommit

--- scripts/precommit.sh
+++ scripts/precommit.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+set -e
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+pushd "$parent_path" > /dev/null
 
 source ./venv/bin/activate
 
+echo "> Updating assets..."
 ls .. -l | grep -P '\d{4}-' --only-matching | sed 's/-//g' | ./generate_grid.py -n 3000 -o ../assets/grid.png -r 2
+echo "> ✅ Grid generated."
 git add ../assets/grid.png
 
-echo "> Grid generated."
+popd > /dev/null
